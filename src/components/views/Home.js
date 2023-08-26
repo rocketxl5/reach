97b6a943c3d7ebd6d5d0cb194b95c2bdf9df8541
Max Gaillard <maxgaillar@gmail.com>
2023-08-25 22:19:24 -0400

    added links to homepage

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <main className="container">
-            <h2><Link to="/signin">Sign in</Link></h2>
-            <h2><Link to="/signup">Sign up</Link></h2>
+            <div style={{ display: "block", width: "30%", padding: "1em" }}>
+                <h2 style={{ display: "block" }}><Link to="/signin">Sign in</Link></h2>
+                <h2 style={{ display: "block" }}><Link to="/signup">Sign up</Link></h2>
+            </div>
         </main>
     )
 }
