deb36bdfe602eb9cf8ddd4a0932d5bfbcfb03c0b
Max Gaillard <maxgaillar@gmail.com>
2023-08-25 22:12:50 -0400

    updated base url. added temp links to homepage

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Signin from '../auth/Signin';
+import React from 'react'
+import Link from 'react-router-dom'
 
 function Home() {
     return (
         <main className="container">
-            <h2>Home</h2>
+            <h2><Link to="/signin">Sign in</Link></h2>
+            <h2><Link to="/signup">Sign up</Link></h2>
         </main>
     )
 }
 
-export default Home;
+export default Home
