59fd04f51a311301c0a95c4d083a05cddc8e8bcf
Max Gaillard <maxgaillar@gmail.com>
2023-08-28 11:12:10 -0400

    Improved navbar component with css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,23 +6,25 @@ function Navbar() {
         <div className="navbar">
             <input type="checkbox" id="mobile-nav" />
             <nav>
-                <h1 className="logo">Reach</h1>
-                <ul>
-                    <li><Link className="nav-link" to="#"><span>Home</span></Link></li>
-                    <li><Link className="nav-link" to="#"><span>Team</span></Link></li>
-                    <li><Link className="nav-link" to="#"><span>About</span></Link></li>
-                    <li><Link className="nav-link" to="#"><span>Contact</span></Link></li>
-                </ul>
+                <section className="left-side">
+                    <h1 className="logo">Reach</h1>
+                    <ul>
+                        <li><Link className="nav-link" to="#"><span>Home</span></Link></li>
+                        <li><Link className="nav-link" to="#"><span>Team</span></Link></li>
+                        <li><Link className="nav-link" to="#"><span>About</span></Link></li>
+                        <li><Link className="nav-link" to="#"><span>Contact</span></Link></li>
+                        <li><Link className="nav-link" to="#"><span>Create account</span></Link></li>
+                    </ul>
+                </section>
+                <section className="right-side">
+                    <Link to="signin">Sign in</Link>
+                    <Link to="signup">Sign up</Link>
+                    <label htmlFor="mobile-nav" className="mobile-nav-label">
+                        <span></span>
+                    </label>
+                </section>
             </nav>
-            <div className="access">
-                <div className="access-links">
-                <Link to="signin">Sign in</Link>
-                <Link to="signup">Sign up</Link>
-            </div>
-            <label htmlFor="mobile-nav" className="mobile-nav-label">
-                <span></span>
-            </label>
-            </div>
+
         </div>
     )
 }
